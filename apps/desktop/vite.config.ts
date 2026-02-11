fc5f0aec330c517af1f253c4cb218eb2ef012ab1
Sarah Lyu <sarah.lyu2012@gmail.com>
2026-02-11 16:58:32 +0800

    Update vite.config.ts

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -9,6 +9,13 @@ export default defineConfig({
   resolve: {
     alias: { '@': resolve(__dirname, 'src') },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
   build: {
     emptyOutDir: true,
     rollupOptions: {
